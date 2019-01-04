98c58f8632d61f218d241efc1a69c526325b0654
darthchudi <chukwudioranu@ymail.com>
2019-01-04 20:18:04 +0100

    feat: add variables for source file and template directories

--- utils/file.go
+++ utils/file.go
@@ -1,6 +1,14 @@
 package utils
 
-import "os"
+import (
+	"os"
+)
+
+// srcDir is the directory that holds the library source files
+var srcDir = "lib/src"
+
+// templateDir is the directory that holds the templates
+var templateDir = "lib/templatates/"
 
 // DoesFileExist checks if a given file exists in the filesystem
 func DoesFileExist(file string) bool {
