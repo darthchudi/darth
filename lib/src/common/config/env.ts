bf5cea13420f3ab152d3bd43306b57f4dbf55268
darthchudi <chukwudioranu@ymail.com>
2019-01-04 19:51:15 +0100

    fix: change typo in port env config

--- lib/src/common/config/env.ts
+++ lib/src/common/config/env.ts
@@ -11,7 +11,7 @@ export default {
   mailgun_domain: process.env.MAILGUN_DOMAIN,
   mailgun_email: process.env.MAILGUN_EMAIL,
   mongodb_uri: process.env.MONGODB_URL,
-  port: Number(process.env.NODE_ENV) || 3009,
+  port: Number(process.env.PORT) || 5006,
   salt_rounds: process.env.SALT_ROUNDS || 10,
   jwt_secret: process.env.JWT_SECRET,
 };
